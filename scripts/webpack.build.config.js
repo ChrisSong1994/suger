e1a25fc1fe8c6cc1bc02ff71277ddca62ca6af7d
jun song <songjun849@gmail.com>
2020-02-18 16:47:31 +0800

    fix:改变导出类库的方式

--- scripts/webpack.build.config.js
+++ scripts/webpack.build.config.js
@@ -13,9 +13,13 @@ module.exports = merge(baseWebpackConfig, {
   output: {
     // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, "../dist"),
-    filename: name + ".js"
+    filename: name + ".js",
+    library: "suger", // 指定类库名,主要用于直接引用的方式(比如使用script 标签)
+    libraryExport: "default", // 对外暴露default属性，就可以直接调用default里的属性
+    globalObject: "this", // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
+    libraryTarget: "umd" // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
+
   },
-  devtool: "inline-source-map",
   plugins: [
     // 引入清理插件
     new CleanWebpackPlugin(),
